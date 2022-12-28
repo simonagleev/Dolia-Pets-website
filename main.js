bc08056f0509440db94491c9b11ae4662c16e01c
Simon <62551180+simonagleev@users.noreply.github.com>
2022-12-28 17:01:55 +0300

    works on ipnones. Removed div container from video

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 // Iphone condition for phone-video-full
 const phoneVideoFull = document.querySelector('.phone-video')
+const pnoneVideo = document.querySelector('.header-right-video')
 const a = navigator.vendor.includes("Apple") || navigator.vendor.includes("apple")
 if(a) {
     console.log('Apple')
     console.log(navigator.vendor)
+    pnoneVideo.classList.add('header-right-video-iphone')
 } else {
     console.log('Not Apple')
     console.log(navigator.vendor)
